a2ccf12784885fe4f766e09a325d291837fe9b01
anik904 <shahriarmahmud904@gmail.com>
2022-02-08 17:43:50 +0600

    7th commit

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // gsap.to(".box-card", { duration: 1, scale: 1, y: 20, });
+///////////////////////
+let i = 0;
+let j = 0;
+let txt = "Now im working to improve my own skill with Front-End Development and Some other section. Extremely motivated to constantly develop my skills and grow professionally, I am confident in my ability to come up with interesting ideas for developing new things.If you want to see my working progress the feel free to check out my code on Github. Have a great day!";
+let txt_head = 'Shahriar Mahmud Anik';
+let speed_head = 130;
+let speed_paragraph = 40;
 
-$('.carousel').carousel()
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("about_me").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed_paragraph);
+    }
+}
+
+function typeHead() {
+    if (j < txt_head.length) {
+        document.getElementById("head").innerHTML += txt_head.charAt(j);
+        j++;
+        setTimeout(typeHead, speed_head);
+    }
+}
+
+
+///////////////////////
