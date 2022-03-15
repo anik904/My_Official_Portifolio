b314757d28a11414d6ad363da7db459ea1f3c3cd
anik904 <shahriarmahmud904@gmail.com>
2022-03-16 00:05:53 +0600

    9th commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let i = 0;
 let j = 0;
 let txt = "Now im working to improve my own skill with Front-End Development and Some other section. Extremely motivated to constantly develop my skills and grow professionally, I am confident in my ability to come up with interesting ideas for developing new things.If you want to see my working progress the feel free to check out my code on Github. Have a great day!";
 let txt_head = 'Shahriar Mahmud Anik';
-let speed_head = 130;
+let speed_head = 60;
 let speed_paragraph = 40;
 
 
@@ -26,3 +26,46 @@ function typeHead() {
 
 
 //////////end typeWrite/////////////
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml2 .letter',
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70 * i
+    }).add({
+        targets: '.ml2',
+        opacity: 0,
+        duration: 2000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+///////////////
+// Initiate an Ajax request on button click
+// $(document).on("click", "button", function() {
+//     // Adding timestamp to set cache false
+//     $.get("" + $.now(), function(data) {
+//         $("body").html(data);
+//     });
+// });
+
+// // Add remove loading class on body element depending on Ajax request status
+// $(document).on({
+//     ajaxStart: function() {
+//         $("body").addClass("loading");
+//     },
+//     ajaxStop: function() {
+//         $("body").removeClass("loading")
+//     }
+// });
+$(document).ready(function() {
+    $("button").click(function() {
+        $("#div1").load("demo_test.txt");
+    });
+});
